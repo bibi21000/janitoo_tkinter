221cf2b2e908e1b75f0b6f8825af11ab1b3a0269
bibi21000 <bibi21000@gmail.com>
2016-01-14 22:26:59 +0100

    remove monkey patch

--- src/janitoo_flask/options.py
+++ src/janitoo_flask/options.py
@@ -24,9 +24,6 @@ __author__ = 'Sébastien GALLET aka bibi21000'
 __email__ = 'bibi21000@gmail.com'
 __copyright__ = "Copyright © 2013-2014-2015 Sébastien GALLET aka bibi21000"
 
-from gevent import monkey
-monkey.patch_all()
-
 import logging
 logger = logging.getLogger(__name__)
 
